bb54389e1c3a5b8c9dc2f95bdcfab3135839c7df
Victor <vicktorteixeira.7@gmail.com>
2022-09-07 18:59:04 -0300

    Command Repository, Dtos and Profiles.

---
+++ CommandsService/CommandsService/Data/ICommandRepo.cs
@@ -0,0 +1,18 @@
+﻿using CommandsService.Models;
+
+namespace CommandsService.Data;
+
+public interface ICommandRepo
+{
+    bool SaveChanges();
+    
+    // Platforms
+    IEnumerable<Platform> GetAllPlatforms();
+    void CreatePlatform(Platform plat);
+    bool PlatformExist(int platformId);
+    
+    // Commands
+    IEnumerable<Command> GetCommandsForPlatform(int platformId);
+    Command GetCommand(int platformId, int commandId);
+    void CreateCommand(int platformId, Command command);
+}
---
+++ CommandsService/CommandsService/Dtos/PlatformReadDto.cs
@@ -0,0 +1,7 @@
+﻿namespace CommandsService.Dtos;
+
+public class PlatformReadDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
