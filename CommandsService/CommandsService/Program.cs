e91741e8fdf955e41c1a641b12b16c827ebaa2dc
Victor <vicktorteixeira.7@gmail.com>
2022-09-08 11:53:06 -0300

    RabbitMQ K8s pre-configuration.

--- CommandsService/CommandsService/Program.cs
+++ CommandsService/CommandsService/Program.cs
@@ -1,9 +1,16 @@
+using CommandsService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseInMemoryDatabase("InMem"));
+builder.Services.AddScoped<ICommandRepo, CommandRepo>();
 
 var app = builder.Build();
 
