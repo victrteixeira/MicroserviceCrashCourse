121eb169e0de35b9a89d205828be4ba1ff81a3bd
Victor <vicktorteixeira.7@gmail.com>
2022-09-12 13:22:06 -0300

    Grpc setup for Platform Service

--- PlatformService/PlatformService/Program.cs
+++ PlatformService/PlatformService/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using PlatformService.AsyncDataServices;
 using PlatformService.Data;
 using PlatformService.Models;
+using PlatformService.SyncDataServices.Grpc;
 using PlatformService.SyncDataServices.Http;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -31,6 +32,7 @@ else
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>(); 
 builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
 builder.Services.AddSingleton<IMessageBusClient, MessageBusClient>();
+builder.Services.AddGrpc();
 
 Console.WriteLine($"--> CommandService Endpoint {builder.Configuration["CommandService"]}");
 
@@ -45,9 +47,20 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapGrpcService<GrpcPlatformService>();
+
+    endpoints.MapGet("/protos/platforms.proto", async context =>
+    {
+        await context.Response.WriteAsync(File.ReadAllText("Protos/Platforms.proto"));
+    });
+});
 
 PrepDb.PrepPopulation(app, app.Environment.IsProduction());
 
